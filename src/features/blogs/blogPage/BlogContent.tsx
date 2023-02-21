1f3cb0bb2560931885d89de9de0cb864e02f3d29
Ogospodigosha <odintsovis@hotmail.com>
2023-02-21 23:07:57 +0300

    add: back to blogs link

--- src/features/blogs/blogPage/BlogContent.tsx
+++ src/features/blogs/blogPage/BlogContent.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect} from 'react';
 import s from './blogPage.module.css'
 import {Box, Breadcrumbs, Divider, Link, Toolbar} from '@mui/material';
-import {useParams} from 'react-router-dom';
+import {NavLink, useParams} from 'react-router-dom';
 import {useSelector} from 'react-redux';
-
+import WestIcon from '@mui/icons-material/West';
 import {AsyncBlogActions, BlogsSelector} from "../index";
 import {useActions} from "../../../utils/useAction";
 
@@ -17,17 +17,23 @@ export const BlogContent = () => {
         }
     }, [blogId])
     return (
-        <Box component="main" sx={{flexGrow: 1, p: 3, backgroundColor: "#DEDBDC", minHeight: "100vh"}}>
+        <Box component="main" sx={{flexGrow: 1, p: 3, backgroundColor: "#DEDBDC", minHeight: "100vh", marginTop: '29px'}}>
             <Toolbar/>
             <Breadcrumbs separator="⏵" aria-label="breadcrumb">
-                <Link underline="none" color="black" href="/" className={s.header}>
+                <Link underline="none" color="black" href="/" className={s.header} >
                     Blogs
                 </Link>
                 <Link underline="none" color="black" className={s.nameBlog}>
                     {blog.name}
                 </Link>
             </Breadcrumbs>
-            <Divider sx={{marginBottom:'23px'}}/>
+            <Divider sx={{marginBottom:'35px', marginTop: '7px'}}/>
+            <NavLink to={'/'} className={s.link}>
+                <div style={{display:'flex', alignItems: 'center'}}>
+                    <WestIcon style={{color: 'black'}}/>
+                    <div style={{marginLeft: '13px'}}>Back to blogs</div>
+                </div>
+            </NavLink>
         </Box>
     );
 };
