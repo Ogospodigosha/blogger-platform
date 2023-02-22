09b542062fa7862e2eda902d22bab9d28ad2f924
Ogospodigosha <odintsovis@hotmail.com>
2023-02-22 15:56:51 +0300

    add: showMore button

--- src/features/blogs/blogPage/BlogContent.tsx
+++ src/features/blogs/blogPage/BlogContent.tsx
@@ -6,13 +6,18 @@ import {useSelector} from 'react-redux';
 import WestIcon from '@mui/icons-material/West';
 import {AsyncBlogActions, BlogsSelector} from "../index";
 import {useActions} from "../../../utils/useAction";
-
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 export const BlogContent = () => {
+    const [showMore, setShowMore] = useState(false);
     const {fetchBlog} = useActions(AsyncBlogActions)
     const blog = useSelector(BlogsSelector.selectBlog)
     const {blogId} = useParams()
     const convertDataFormat = (value: string) => {
         return new Intl.DateTimeFormat('ru-RU').format(new Date(value))
+    }
+    const clickHandler = () =>{
+        setShowMore(!showMore)
     }
     console.log(blog.createdAt)
     useEffect(() => {
@@ -42,14 +47,18 @@ export const BlogContent = () => {
             <div style={{width: '100%', height: '312px', backgroundColor: 'white', marginBottom: '28px'}}></div>
             <div style={{display: 'flex'}}>
                 <div className={s.img}></div>
-                <div>
+                <div style={{marginBottom:'23px'}}>
                     <div className={s.name}>{blog.name}</div>
                     <div className={s.date}><span className={s.spanDate}>blog creation date:</span>{blog.createdAt !== undefined ? convertDataFormat(blog.createdAt): '' }</div>
                     <div className={s.website}>Website:<a className={s.url} href={blog.websiteUrl}>{blog.websiteUrl}</a></div>
-                    <div className={s.description}>{blog.description}</div>
+                    <div className={s.description}>{showMore ? blog.description :`${blog.description && blog.description.substring(0, 200)}`}</div>
+                    {blog.description && blog.description.length > 130 && <div className={s.btn} onClick={clickHandler}>
+                        <div>{showMore ? "Show less" : "Show more"}</div>
+                        {showMore ?<ExpandLessIcon/>: <ExpandMoreIcon/>}
+                    </div>}
                 </div>
-
             </div>
+            <Divider variant="fullWidth"/>
         </Box>
     );
 };
